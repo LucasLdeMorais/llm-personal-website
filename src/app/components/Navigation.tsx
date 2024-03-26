90a6829741f3035899ba90ed8ab34ee302be7bcc
LucasLdeMorais <devlucasldemorais@gmail.com>
2024-03-26 19:33:10 -0300

    feature: navigation component

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -4,27 +4,37 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styled from "styled-components";
 import pages from "../constants/pages";
+import { useThemeContext } from "./ThemedLayout";
 
 export default function Navigation () {
     const pathname = usePathname();
+    const { currentTheme } = useThemeContext();
 
-    const StyledListItem = styled.li`
-        display: inline-block;
-        margin: 5px;
+    const StyledList = styled.ul`
+        padding: 0;
+        margin: 0 0 10px 0;
+        
+        & li {
+            display: inline-block;
+            padding: 5px;
+        }
+    `
+
+    const StyledNav = styled.nav`
+        width: 100%;
+        bottom: 0;
+        position: relative;
     `
 
     return (<>
-        <nav>
-            <ul>
-                <ul>
-                    {pages.map(page => {
-                        return <StyledListItem key={page.pageName}>
-                            <Link href={page.path} className={`link ${pathname === '/' ? 'active' : ''}`}>{page.pageName}</Link>
-                        </StyledListItem>
-                    })}
-                </ul>
-            </ul>
-        </nav>
-        
+        <StyledNav>
+            <StyledList>
+                {pages.map(page => {
+                    return <li key={page.pageName}>
+                        <Link href={page.path} className={`link ${pathname === '/' ? 'active' : ''}`}>{page.pageName}</Link>
+                    </li>
+                })}
+            </StyledList>
+        </StyledNav>
     </>)
 }
